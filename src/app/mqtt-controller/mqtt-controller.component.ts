9f64dbdecdb07531aa3a4981d6d0dcd390fe7585
Nathan <lordnthan0@gmail.com>
2018-11-10 16:05:31 -0600

    updated to send commands to controller/

--- src/app/mqtt-controller/mqtt-controller.component.ts
+++ src/app/mqtt-controller/mqtt-controller.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { JoystickServiceService } from '../JoystickService/joystick-service.service';
+import { timer, Observable, Subscription, fromEventPattern } from 'rxjs';
+import {
+  IMqttMessage,
+  MqttService,
+  MqttModule,
+  IMqttServiceOptions
+} from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '192.168.0.200',
+  port: 9001,
+  protocol: 'ws'
+};
+
+const intervalDelay = 250;
 
 @Component({
   selector: 'app-mqtt-controller',
@@ -6,7 +22,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mqtt-controller.component.css']
 })
 export class MqttControllerComponent implements OnInit {
+  private cmdTimer: Observable<any>;
+  private cmdTimerSub: Subscription;
+  private mqttClient: MqttService;
+  joystick: JoystickServiceService;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.joystick = new JoystickServiceService();
+    this.cmdTimer = timer(0, intervalDelay);
+    this.cmdTimerSub = this.cmdTimer.subscribe(this.cmdSend.bind(this));
+    this.mqttClient = new MqttService(MQTT_SERVICE_OPTIONS);
+  }
+  OnDestroy() {
+    this.cmdTimerSub.unsubscribe();
+    this.mqttClient.disconnect();
+  }
+
+  cmdSend() {
+    const msg: String =
+      'x = ' +
+      this.joystick.x.toString() +
+      ' y = ' +
+      this.joystick.y.toString();
+
+    this.mqttClient
+      .publish('controller/x', this.joystick.x.toString())
+      .subscribe();
+    this.mqttClient
+      .publish('controller/y', this.joystick.y.toString())
+      .subscribe();
+  }
 }
